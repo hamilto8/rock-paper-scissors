edf1dbe35e37e1c37696644a3ff6e133a6df6e32
Michael Hamilton <michael.ts.hamilton@gmail.com>
2019-08-28 11:34:45 -0500

    Began preliminary work on adding a graphical interface

--- index.js
+++ index.js
@@ -1,58 +1,118 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const start = document.querySelector('#start');
+const display = document.querySelector('#display');
+const count_view = document.querySelector('#count_view');
+const attack = document.querySelector('#attack');
+const computer_score = document.querySelector('#computer_score');
+const player_score = document.querySelector('#player_score');
+let started = false;
+let count = 0;
+
+count_view.textContent = `Count: ${count}`;
+
+start.addEventListener('click', ()=>{
+    if(started === false){
+        started = true;
+        start.textContent = 'Reset';
+        attack.textContent = ``;
+    } else {
+        count = 0;
+        attack.textContent = ``
+        count_view.textContent = `Count: ${count}`;
+    }
+    game();
+});
+
 function computerPlay(){
     let attackStyle = ['rock', 'paper', 'scissors'];
     return attackStyle[Math.floor(Math.random()*attackStyle.length)];
 }
 
 function game(){
-    let count = 0;
     let computerScore = 0;
     let playerScore = 0;
+    
+    
+        rock.addEventListener('click', playRock);
+        paper.addEventListener('click', playPaper);
+        scissors.addEventListener('click', playScissors);
 
-    function playRound(playerSelection, computerSelection){
-        if(playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
-            computerScore+=1;
-            return "You lose! Paper beats rock."
-        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === "paper") {
-            playerScore+=1;
-            return "You win! Computer chose poorly."
-        } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "rock"){
-            computerScore+=1;
-            return "You lose! Rock beats scissors."
-        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors'){
-            playerScore+=1;
-            return "You win! Computer chose poorly."
-        } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
-            playerScore+=1;
-            return "You win! Computer chose poorly."
-        } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
-            computerScore+=1;
-            return "You lose! Paper beats rock"
-        } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper'){
-            playerScore+=1;
-            return "You win! Computer chose poorly"
-        } else if (playerSelection.toLowerCase() === 'paper' && computerPlay === 'scissors'){
-            computerScore+=1;
-            return "You lose! Scissors beats paper"
-        } else if (playerSelection.toLowerCase() === computerSelection){
-            return "It's a tie!"
-        } else {
-            return "Huh?"
+        function playRock(){
+            if(count >=5){
+                attack.textContent = 'You have reached the maximum count';
+            } else {
+                let computerSelection = computerPlay();
+                count++;
+                count_view.textContent = `Count: ${count}`;
+                playRound('rock', computerSelection);
+                attack.textContent = `Player Chose: rock | Computer Chose: ${computerSelection}`;
+
+                computer_score.textContent = computerScore;
+                player_score.textContent = playerScore;
+            }
         }
-    }
 
+        function playPaper(){
+            if(count >=5){
+                attack.textContent = 'You have reached the maximum count';
+            } else {
+                count++;
+                count_view.textContent = `Count: ${count}`;
+                let computerSelection = computerPlay();
+                playRound('paper', computerSelection);
+                attack.textContent = `Player Chose: paper | Computer Chose: ${computerSelection}`;
 
-    while (count < 5){
-        let playerSelection = prompt("Choice?");
-        let computerSelection = computerPlay();
-        console.log(`
-        Player Chose: ${playerSelection},
-        Computer Chose: ${computerSelection}
-        `);
-        console.log(playRound(playerSelection, computerSelection))
-        count++;
-    }
-    console.log(`
-    Player Score: ${playerScore}, 
-    Computer Score: ${computerScore}
-    `);
+                computer_score.textContent = computerScore;
+                player_score.textContent = playerScore;
+            }
+        }
+
+        function playScissors(){
+            if (count >= 5){
+                attack.textContent = 'You have reached the maximum count';
+            } else {
+                count++;
+                count_view.textContent = `Count: ${count}`;
+                let computerSelection = computerPlay();
+                playRound('scissors', computerSelection);
+                attack.textContent = `Player Chose: scissors | Computer Chose: ${computerSelection}`;
+ 
+                computer_score.textContent = computerScore;
+                player_score.textContent = playerScore;
+            }
+        }
+
+        function playRound(playerSelection, computerSelection){
+            if(playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
+                computerScore+=1;
+                return "You lose! Paper beats rock."
+            } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === "paper") {
+                playerScore+=1;
+                return "You win! Computer chose poorly."
+            } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "rock"){
+                computerScore+=1;
+                return "You lose! Rock beats scissors."
+            } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors'){
+                playerScore+=1;
+                return "You win! Computer chose poorly."
+            } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
+                playerScore+=1;
+                return "You win! Computer chose poorly."
+            } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
+                computerScore+=1;
+                return "You lose! Paper beats rock"
+            } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper'){
+                playerScore+=1;
+                return "You win! Computer chose poorly"
+            } else if (playerSelection.toLowerCase() === 'paper' && computerPlay === 'scissors'){
+                computerScore+=1;
+                return "You lose! Scissors beats paper"
+            } else if (playerSelection.toLowerCase() === computerSelection){
+                return "It's a tie!"
+            } else {
+                return "Huh? Please Enter Rock, Paper or Scissors."
+            }
+        } 
 }
